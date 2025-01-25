20f8c8e94a6f49b6ddc4578dabc4ebf3494d1d14
Ksenua <Gagina>
2025-01-25 15:48:37 +0300

    Initial Commit

---
+++ PhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneBook
+//
+//  Created by Ксения Гагина on 25.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
